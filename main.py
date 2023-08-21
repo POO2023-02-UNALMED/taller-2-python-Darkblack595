f81f9b44f358da3cd008193018942662dae75bc4
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2023-08-20 20:25:57 -0500

    decimo5

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class Auto:
     def verificarIntegridad(self):
         if self.registro == self.motor.registro:
             for i in range(len(self.asientos)):
-                if self.asientos is not None:
+                if self.asientos[i] is not None:
                     if self.asientos[i].registro != self.registro:
                         return "Las piezas no son originales"
             return "Auto original"
